b819a3c8312e976844816ba64787cb05b387f4e6
drumnaoto <drumnaoto@gmail.com>
2017-11-02 15:54:34 +0900

    add-edit-and-update-action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(user_params)
+    end
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
