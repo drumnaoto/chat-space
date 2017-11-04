b93226da8bc05b29138da2c187e1f389e79b058c
drumnaoto <drumnaoto@gmail.com>
2017-11-04 15:53:37 +0900

    create-message-model
    モデルを作成し、
    マイグレーションファイルを編集し、
    テーブルを作成しようとして、
    マイグレーションファイルの実行をしたところ、
    「同じ名前のマイグレーションファイルが重複している」というエラーが出たため、
    過去の不要なマイグレーションファイルを削除して、
    マイグレーションファイルの実行をしました。

--- db/migrate/20171104054136_create_messages.rb
+++ db/migrate/20171104054136_create_messages.rb
@@ -1,6 +1,8 @@
-class CreateMembers < ActiveRecord::Migration[5.0]
+class CreateMessages < ActiveRecord::Migration[5.0]
   def change
-    create_table :members do |t|
+    create_table :messages do |t|
+      t.text :body
+      t.string :image
       t.references :user, null: false, foreign_key: true
       t.references :group, null: false, foreign_key: true
       t.timestamps
