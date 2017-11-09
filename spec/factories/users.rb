ad36801b0ddb0e93bb3b2d801b345706f77736b7
drumnaoto <drumnaoto@gmail.com>
2017-11-09 12:20:44 +0900

    test-at-user-model
    userモデルでのテスト。

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name                  "naoto"
+    email                 "naoto@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+  end
+
+end
