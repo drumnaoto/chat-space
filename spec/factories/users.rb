b41adedcb9b8375127b0c3f1bf5cdc789b9056b0
drumnaoto <drumnaoto@gmail.com>
2017-11-09 13:24:37 +0900

    edit-user-by-faker
    fakerを使って、userのnameとemailをランダムに作成しました。
    passwordはpassword_confirmationと同じ値にするためにfakerは使用していません。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
 
   factory :user do
-    name                  "naoto"
-    email                 "naoto@gmail.com"
-    password              "00000000"
-    password_confirmation "00000000"
+    name                   { Faker::Name.name }
+    email                  { Faker::Internet.email }
+    password               "00000000"
+    password_confirmation  "00000000"
   end
 
 end
