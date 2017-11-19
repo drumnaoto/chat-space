d784f063fe0a5febe6483fdb5fa288e21ff34917
drumnaoto <drumnaoto@gmail.com>
2017-11-19 20:02:08 +0900

    edit-css_final
    デプロイ環境で、
    バッククォートが使えないので、
    シングルクォートに書き直しが必要だった為。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,26 +2,26 @@ $(document).on('turbolinks:load', function () {
 
   var search_list = $("#user-search-result");
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name"> ${user.name} </p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-                </div>`
+    var html = '<div class="chat-group-user clearfix">'+
+                  '<p class="chat-group-user__name">'+user.name+'</p>'+
+                  '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="'+user.id+'" data-user-name="'+user.name+'">追加</a>'+
+                '</div>'
     search_list.append(html);
   }
   function appendNoUser(user) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name"> ${ user } </p>
-                </div>`
+    var html = '<div class="chat-group-user clearfix"'+
+                  '<p class="chat-group-user__name">'+user+'</p>'+
+               '</div>'
     search_list.append(html);
   }
 
   var member_list = $("#chat-group-users");
   function appendMember(id, name) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
-                  <input name='group[user_ids][]' type='hidden' value='${id}'>
-                  <p class='chat-group-user__name'>${ name }</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-                </div>`
+    var html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-'+id+'">'+
+                  '<input name="group[user_ids][]" type="hidden" value="'+id+'">'+
+                  '<p class="chat-group-user__name">'+name+'</p>'+
+                  '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>'+
+                '</div>'
     member_list.append(html);
   };
 
