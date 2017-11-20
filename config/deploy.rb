e5d370bf4aa098913c28224416a909fa938dda0d
drumnaoto <drumnaoto@gmail.com>
2017-11-20 15:14:18 +0900

    Revert "modify deploy order of capistrano"
    This reverts commit 3bbb35cef5cf97e2ffe4c7f19ca2f10502c9c0b3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:drumnaoto/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
@@ -15,11 +14,11 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
 end
+
+set :linked_files, %w{ config/secrets.yml }
